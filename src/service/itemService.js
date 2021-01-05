5f56096e50167fd136094b09b98c8247a452c075
Ben Freudman <bfreud94@gmail.com>
2021-01-04 21:20:05 -0500

    Enhanced error handling

--- src/service/itemService.js
+++ src/service/itemService.js
@@ -24,67 +24,95 @@ const authenticateUser = async (token) => {
     }
 };
 
-const findItems = async (id) => Item.find({ userId: id });
-
-const addItem = async (userId, { name, cost, purchaseByDate, linkToProduct }, itemURL) => {
-    const item = new Item({
-        userId,
-        name,
-        cost,
-        dateAdded: new Date(),
-        purchaseByDate,
-        linkToProduct,
-        itemURL
-    });
-    return item.save();
+const getItems = async (id, next) => {
+    try {
+        return Item.find({ userId: id });
+    } catch (err) {
+        next(new Error('Failed to get items from MongoDB'));
+    }
+    return null;
 };
 
-const deleteItem = async (id) => Item.findByIdAndDelete(id);
-
-const updateItem = async (userId, _id, { name, cost, dateAdded, purchaseByDate, linkToProduct }) => {
-    const query = { _id };
-    const updatedItem = {
-        userId,
-        _id,
-        name,
-        cost,
-        dateAdded,
-        purchaseByDate,
-        linkToProduct
-    };
-    return Item.findOneAndUpdate(query, updatedItem, { useFindAndModify: false });
+const addItem = async (userId, { name, cost, purchaseByDate, linkToProduct }, itemURL, next) => {
+    try {
+        const item = new Item({
+            userId,
+            name,
+            cost,
+            dateAdded: new Date(),
+            purchaseByDate,
+            linkToProduct,
+            itemURL
+        });
+        return item.save();
+    } catch (err) {
+        next(new Error('Failed to add item to MongoDB'));
+    }
+    return null;
 };
 
-const addItemImageToGoogleCloud = async (name, file) => {
-    const googleUrl = process.env.GOOGLE_URL;
-    const bucket = storage.bucket(bucketName);
-    const buffer = Buffer.from(file.data, 'base64');
-    const readable = new Readable();
-    readable._read = () => {};
-    readable.push(buffer);
-    readable.push(null);
-    const fileExtension = file.name.split('.')[1];
-    await new Promise((res) => readable
-        .pipe(
-            bucket.file(`${name}.${fileExtension}`).createWriteStream({
-                resumable: false,
-                gzip: true
-            })
-        )
-        .on('finish', res));
-    return `${googleUrl}/${bucketName}/${name}.${fileExtension}`;
+const deleteItem = async (id, next) => {
+    try {
+        return Item.findByIdAndDelete(id);
+    } catch (err) {
+        next(new Error('Failed to delete item from MongoDB'));
+    }
+    return null;
 };
 
-const deleteItemImageFromGoogleCloud = async (filePath) => {
+const updateItem = async (userId, _id, { name, cost, dateAdded, purchaseByDate, linkToProduct }, next) => {
+    try {
+        const query = { _id };
+        const updatedItem = {
+            userId,
+            _id,
+            name,
+            cost,
+            dateAdded,
+            purchaseByDate,
+            linkToProduct
+        };
+        return Item.findOneAndUpdate(query, updatedItem, { useFindAndModify: false });
+    } catch (err) {
+        next(new Error('Failed to update item from MongoDB'));
+    }
+    return null;
+};
+
+const addItemImageToGoogleCloud = async (name, file, next) => {
+    try {
+        const googleUrl = process.env.GOOGLE_URL;
+        const bucket = storage.bucket(bucketName);
+        const buffer = Buffer.from(file.data, 'base64');
+        const readable = new Readable();
+        readable._read = () => {};
+        readable.push(buffer);
+        readable.push(null);
+        const fileExtension = file.name.split('.')[1];
+        await new Promise((res) => readable
+            .pipe(
+                bucket.file(`${name}.${fileExtension}`).createWriteStream({
+                    resumable: false,
+                    gzip: true
+                })
+            )
+            .on('finish', res));
+        return `${googleUrl}/${bucketName}/${name}.${fileExtension}`;
+    } catch (err) {
+        next(new Error('Failed to add image to GCS'));
+    }
+    return null;
+};
+
+const deleteItemImageFromGoogleCloud = async (filePath, next) => {
     try {
         const bucket = storage.bucket(bucketName);
         const deleted = await bucket.file(filePath).delete();
         return deleted;
     } catch (err) {
-        // eslint-disable-next-line no-console
-        console.log(err);
-        return false;
+        next(new Error('Failed to delete item from GCS'));
     }
+    return null;
 };
 
 module.exports = {
@@ -93,6 +121,6 @@ module.exports = {
     authenticateUser,
     deleteItem,
     deleteItemImageFromGoogleCloud,
-    findItems,
+    getItems,
     updateItem
 };
